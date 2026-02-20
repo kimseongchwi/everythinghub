cdb8385adac19e545e5b96824516b06520618d4f
kimseongchwi <ghfkddl665@naver.com>
2026-02-20 10:55:12 +0900

    fix: 우선순위

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4390230382155372"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
       </head>
       <ConditionalNavigation notoSansKrClassName={notoSansKr.className}>
